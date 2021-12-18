c6c13cfc1b2b81a28460ac487a9dab5857867611
Marcin Hodor <mhodor@o2.pl>
2021-12-18 20:45:41 +0100

    api > add return tutle & image_url

--- api/app.py
+++ api/app.py
@@ -5,17 +5,16 @@ from config import Config
 
 g = Goose({'enable_image_fetching': True})
 
-def get_article_text(url):
-    article_text = g.extract(url=url)
-    return article_text.cleaned_text
+def get_article_data(url):
+    data = g.extract(url=url)
+    return data
 
-def get_article_summary(article_text):
+def get_article_summary(text):
     API_URL = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6"
     headers = {"Authorization": f"Bearer {app.config['API_TOKEN']}"}
-    response = requests.post(API_URL, headers=headers, json=article_text)
-    summary_text = response.json()
-    return summary_text
-
+    response = requests.post(API_URL, headers=headers, json=text)
+    text_summary = response.json()
+    return text_summary
 
 # Flask server
 app = Flask(__name__)
@@ -24,8 +23,12 @@ app.config.from_object(Config)
 @app.route('/api', methods=['GET', 'POST'])
 def api():
     if request.method == 'POST':
-        article_text = get_article_text(request.json['url'])
-        article_summary = get_article_summary(article_text)
-        return {'summary': article_summary}
+        article_data = get_article_data(request.json['url'])
+        article_summary = get_article_summary(article_data.cleaned_text)
+        return {
+            'title': article_data.title,
+            'image_url': article_data.top_image.src,
+            'summary': article_summary
+            }
     else:
         return '<p>API</p>'
