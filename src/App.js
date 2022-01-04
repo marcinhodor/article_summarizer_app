446d15b470f88102af15db7e75ced351bef4f5cd
Marcin Hodor <mhodor@o2.pl>
2022-01-04 22:06:57 +0100

    Add and style input form and article card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
   const [url, setUrl] = useState("");
-  const [site, setSite] = useState("");
-  const [articleDataFromUrl, setArticleDataFromUrl] = useState({});
+  const [articleDataFromUrl, setArticleDataFromUrl] = useState();
   const [isLoadingFromUrl, setIsLoadingFromUrl] = useState(false);
 
   const handleSubmitFromUrl = async (e) => {
@@ -28,46 +27,56 @@ function App() {
     setIsLoadingFromUrl(false);
   };
 
-  useEffect(() => {
-    if (!site) return;
-    if (site === "fox-news") {
-    }
-
-    console.log("effect", site);
-  }, [site]);
-
   return (
-    <div className="container mx-auto font-serif">
+    <div className="container px-2 mx-auto font-serif">
       <header>
-        <h1 className="text-3xl">Article Summarizer</h1>
+        <h1 className="my-6 text-3xl text-center">Article Summarizer</h1>
       </header>
       <main>
         <section>
-          <form onSubmit={handleSubmitFromUrl} className="flex">
-            <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Enter URL:
+          <form onSubmit={handleSubmitFromUrl} className="flex flex-col mb-6">
+            <div className="flex mb-3">
+              <span className="inline-flex items-center px-3 text-sm text-gray-500 bg-white border-t border-b border-l border-gray-300 shadow-sm rounded-l-md">
+                URL:
+              </span>
               <input
                 type="text"
+                placeholder="Enter link here"
                 value={url}
                 onChange={(e) => setUrl(e.target.value)}
+                className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-r-lg shadow-sm appearance-none"
               />
-            </label>
-            <br />
-            <input type="submit" />
-          </form>
-          {articleDataFromUrl && !isLoadingFromUrl ? (
-            <div>
-              <p>{articleDataFromUrl.summary}</p>
             </div>
-          ) : null}
-          {isLoadingFromUrl ? <p>Loading...</p> : null}
+            <input
+              type="submit"
+              className="cursor-pointer text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300  rounded-lg px-5 py-2.5 mb-2 w-24"
+            />
+          </form>
         </section>
         <section>
-          <select name="site-select" onChange={(e) => setSite(e.target.value)}>
-            <option value="">Choose a site...</option>
-            <option value="fox-news">Fox News</option>
-          </select>
-          <p>Site: {site}</p>
+          {articleDataFromUrl && !isLoadingFromUrl ? (
+            <a
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row hover:bg-gray-100"
+            >
+              <img
+                className="max-w-sm sm:rounded-none lg:rounded-l-lg"
+                src={articleDataFromUrl.image_url}
+                alt="article"
+              />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                  {articleDataFromUrl.title}
+                </h5>
+                <p className="mb-3 font-normal text-gray-700">
+                  {articleDataFromUrl.summary}
+                </p>
+              </div>
+            </a>
+          ) : null}
+          {isLoadingFromUrl ? <p>Loading...</p> : null}
         </section>
       </main>
     </div>
