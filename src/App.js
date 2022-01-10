4d7e8b2d9d5418c9c9a00e80fd226e759fee8ec5
Marcin Hodor <mhodor@o2.pl>
2022-01-10 23:04:36 +0100

    add loading spinner, add error hadling, add handle focus

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { useState } from "react";
+import { Oval } from "react-loader-spinner";
 
 function App() {
   const [url, setUrl] = useState("");
+  const [errorMsg, setErrorMsg] = useState();
   const [articleDataFromUrl, setArticleDataFromUrl] = useState();
   const [isLoadingFromUrl, setIsLoadingFromUrl] = useState(false);
 
   const handleSubmitFromUrl = async (e) => {
     e.preventDefault();
+    if (url.length <= 4) {
+      setErrorMsg("Url has to be at least 5 characters long.");
+      return;
+    }
+    // Fetch request
     setIsLoadingFromUrl(true);
+    setErrorMsg();
+
     const response = await fetch("/api", {
       method: "POST",
       headers: {
@@ -17,13 +26,40 @@ function App() {
       body: JSON.stringify({ url }),
     });
 
-    const jsonResponse = await response.json();
+    // Error handling #1
+    const handleError = () => {
+      setErrorMsg(
+        "Something went wrong. Check if the URL provided is correct."
+      );
+      setIsLoadingFromUrl(false);
+    };
+
+    if (!response.ok) {
+      handleError();
+      return;
+    }
+
+    // Response
+    const data = await response.json();
+
+    // Error handling #2
+    if (data.title === "Page not found") {
+      setArticleDataFromUrl();
+      handleError();
+      return;
+    }
+
+    // Assign fetched data to variable
     setArticleDataFromUrl({
-      title: jsonResponse.title,
-      image_url: jsonResponse.image_url,
-      summary: jsonResponse.summary[0].summary_text,
+      title: data.title,
+      image_url: data.image_url,
+      summary: data.summary[0].summary_text,
     });
-    console.log(articleDataFromUrl);
+    setIsLoadingFromUrl(false);
+  };
+
+  const handleFocus = () => {
+    setErrorMsg();
     setIsLoadingFromUrl(false);
   };
 
@@ -35,7 +71,7 @@ function App() {
       <main>
         <section>
           <form onSubmit={handleSubmitFromUrl} className="flex flex-col mb-6">
-            <div className="flex mb-3">
+            <div className="flex mb-1">
               <span className="inline-flex items-center px-3 text-sm text-gray-500 bg-white border-t border-b border-l border-gray-300 shadow-sm rounded-l-md">
                 URL:
               </span>
@@ -44,16 +80,18 @@ function App() {
                 placeholder="Enter link here"
                 value={url}
                 onChange={(e) => setUrl(e.target.value)}
+                onFocus={handleFocus}
                 className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-r-lg shadow-sm appearance-none"
               />
             </div>
+            {errorMsg ? <p className="text-red-600 ">{errorMsg}</p> : null}
             <input
               type="submit"
-              className="cursor-pointer text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300  rounded-lg px-5 py-2.5 mb-2 w-24"
+              className="mt-3 cursor-pointer text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300  rounded-lg px-5 py-2.5 mb-2 w-24"
             />
           </form>
         </section>
-        <section>
+        <section className="flex justify-center">
           {articleDataFromUrl && !isLoadingFromUrl ? (
             <a
               href={url}
@@ -76,7 +114,9 @@ function App() {
               </div>
             </a>
           ) : null}
-          {isLoadingFromUrl ? <p>Loading...</p> : null}
+          {isLoadingFromUrl ? (
+            <Oval arialLabel="loading-indicator" color="#555" />
+          ) : null}
         </section>
       </main>
     </div>
